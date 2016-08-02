2c7911c9a6af9bc4a49df8fbcd7d506ad8f06027
kmnip <kamingnip@gmail.com>
2016-08-02 14:39:34 -0700

    test code

--- src/rnabloom/RNABloom.java
+++ src/rnabloom/RNABloom.java
@@ -8,6 +8,7 @@ package rnabloom;
 import rnabloom.bloom.CountingBloomFilter;
 import static rnabloom.util.SequenceOperations.*;
 import static java.lang.Math.pow;
+import rnabloom.bloom.BloomFilter;
 
 /**
  *
@@ -26,7 +27,20 @@ public class RNABloom {
         // TODO code application logic here
         
         String kmer = "AAAAATTTTTCCCCCGGGGG";
-        System.out.println(smallestStrand(kmer));
+        String kmer2 = "AACAATTTTTCCCCCGGGGG";
+        
+        BloomFilter f1 = new BloomFilter(NUM_BITS_1GB, 3, 689, kmer.length());
+        f1.add(kmer);
+        System.out.println(f1.lookup(kmer));
+        System.out.println(f1.lookup(kmer2));
+        
+        CountingBloomFilter f2 = new CountingBloomFilter((int) NUM_BYTES_1GB, 3, 689, kmer.length());
+        
+        for (int i=0; i < 20; ++i) {
+            f2.increment(kmer);
+        }
+        
+        System.out.println(f2.getCount(kmer));
     }
     
 }
