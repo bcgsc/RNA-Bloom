96796906fe715971e6751398b20489e7a17a78e9
kmnip <kamingnip@gmail.com>
2016-08-09 14:10:44 -0700

    methods to greedily extend one step

--- src/rnabloom/util/GraphUtils.java
+++ src/rnabloom/util/GraphUtils.java
@@ -7,7 +7,6 @@ package rnabloom.util;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import rnabloom.graph.BloomFilterDeBruijnGraph;
 import rnabloom.graph.BloomFilterDeBruijnGraph.Kmer;
 
@@ -17,7 +16,7 @@ import rnabloom.graph.BloomFilterDeBruijnGraph.Kmer;
  */
 public final class GraphUtils {
     
-    public static Kmer greedyExtendRightOnce(BloomFilterDeBruijnGraph graph, String source, int lookahead) {
+    public static Kmer greedyExtendRightOnce(BloomFilterDeBruijnGraph graph, Kmer source, int lookahead) {
         ArrayList<Kmer> candidates = graph.getSuccessors(source);
         
         if (candidates.isEmpty()) {
@@ -28,11 +27,119 @@ public final class GraphUtils {
                 return candidates.get(0);
             }
             else {
-                Kmer best = null;
+                ArrayList<Kmer> alts = graph.getSuccessors(source);
+                Kmer cursor = alts.remove(alts.size()-1);
                 
-                /**@TODO */
+                ArrayList<Kmer> path = new ArrayList<>(lookahead); 
+                path.add(cursor);
                 
-                return best;
+                ArrayList<ArrayList<Kmer>> frontier = new ArrayList<>(lookahead);
+                frontier.add(alts);
+                
+                float bestCov = 0;
+                int bestLen = 1;
+                ArrayList<Kmer> bestPath = path;
+                
+                while (!frontier.isEmpty()) {
+                    if (path.size() < lookahead) {
+                        alts = graph.getSuccessors(cursor);
+                        if (!alts.isEmpty()) {
+                            cursor = alts.remove(alts.size()-1);
+                            path.add(cursor);
+                            frontier.add(alts);
+                            continue;
+                        }
+                    }
+                    
+                    float pathCov = graph.getMeanKmerCoverage(path);
+                    int pathLen = path.size();
+                    if (bestLen < pathLen || bestCov < pathCov) {
+                        bestPath = new ArrayList<>(path);
+                        bestCov = pathCov;
+                        bestLen = pathLen;
+                    }
+
+                    int i = path.size()-1;
+                    while (i >= 0) {
+                        alts = frontier.get(i);
+                        path.remove(i);
+                        if (alts.isEmpty()) {
+                            frontier.remove(i);
+                            --i;
+                        }
+                        else {
+                            cursor = alts.remove(alts.size()-1);
+                            path.add(cursor);
+                            break;
+                        }
+                    }
+                }
+                
+                return bestPath.get(0);
+            }
+        }
+    }
+    
+    public static Kmer greedyExtendLeftOnce(BloomFilterDeBruijnGraph graph, Kmer source, int lookahead) {
+        ArrayList<Kmer> candidates = graph.getPredecessors(source);
+        
+        if (candidates.isEmpty()) {
+            return null;
+        }
+        else {
+            if (candidates.size() == 1) {
+                return candidates.get(0);
+            }
+            else {
+                ArrayList<Kmer> alts = graph.getPredecessors(source);
+                Kmer cursor = alts.remove(alts.size()-1);
+                
+                ArrayList<Kmer> path = new ArrayList<>(lookahead); 
+                path.add(cursor);
+                
+                ArrayList<ArrayList<Kmer>> frontier = new ArrayList<>(lookahead);
+                frontier.add(alts);
+                
+                float bestCov = 0;
+                int bestLen = 1;
+                ArrayList<Kmer> bestPath = path;
+                
+                while (!frontier.isEmpty()) {
+                    if (path.size() < lookahead) {
+                        alts = graph.getPredecessors(cursor);
+                        if (!alts.isEmpty()) {
+                            cursor = alts.remove(alts.size()-1);
+                            path.add(cursor);
+                            frontier.add(alts);
+                            continue;
+                        }
+                    }
+                    
+                    float pathCov = graph.getMeanKmerCoverage(path);
+                    int pathLen = path.size();
+                    if (bestLen < pathLen || bestCov < pathCov) {
+                        bestPath = new ArrayList<>(path);
+                        bestCov = pathCov;
+                        bestLen = pathLen;
+                    }
+
+                    int i = path.size()-1;
+                    while (i >= 0) {
+                        alts = frontier.get(i);
+                        path.remove(i);
+                        if (alts.isEmpty()) {
+                            frontier.remove(i);
+                            --i;
+                        }
+                        else {
+                            cursor = alts.remove(alts.size()-1);
+                            path.add(cursor);
+                            break;
+                        }
+                    }
+                }
+                
+                return bestPath.get(0);
             }
         }
     }
