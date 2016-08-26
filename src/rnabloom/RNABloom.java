7163ea83db6d32c77c57370b029b1fc82d16fa58
kmnip <kamingnip@gmail.com>
2016-08-25 18:29:56 -0700

    methods for creating DBG and assembling fragments

--- src/rnabloom/RNABloom.java
+++ src/rnabloom/RNABloom.java
@@ -10,20 +10,14 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import static java.lang.Math.pow;
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.NoSuchElementException;
 import java.util.regex.Pattern;
-import java.util.stream.Stream;
 import java.util.zip.GZIPInputStream;
 import rnabloom.graph.BloomFilterDeBruijnGraph;
-import rnabloom.graph.BloomFilterDeBruijnGraph.Kmer;
+import rnabloom.io.FastqPairReader;
+import rnabloom.io.FastqPairReader.ReadPair;
 import rnabloom.io.FastqReader;
-import rnabloom.io.FastqRecord;
-import static rnabloom.util.GraphUtils.assemble;
 import static rnabloom.util.GraphUtils.assembleFragment;
-import static rnabloom.util.GraphUtils.correctMismatches;
-import static rnabloom.util.GraphUtils.getMaxCoveragePath;
 import static rnabloom.util.SeqUtils.*;
 
 /**
@@ -35,6 +29,128 @@ public class RNABloom {
     
     public final static long NUM_BITS_1GB = (long) pow(1024, 3) * 8;
     public final static long NUM_BYTES_1GB = (long) pow(1024, 3);
+    private final static String GZIP_EXTENSION = ".gz";
+    
+    private final int k;
+    private final Pattern qualPattern;
+    private final BloomFilterDeBruijnGraph graph;
+    
+    public RNABloom(boolean strandSpecific, long dbgbfSize, long cbfSize, int dbgbfNumHash, int cbfNumHash, int seed, int k, int q) {
+        this.k = k;
+        this.qualPattern = getPhred33Pattern(q, k);
+        
+        graph = new BloomFilterDeBruijnGraph(dbgbfSize,
+                                            cbfSize,
+                                            dbgbfNumHash,
+                                            cbfNumHash,
+                                            seed,
+                                            k,
+                                            strandSpecific);
+    }
+    
+    public void createDBG(String[] forwardFastqs, String[] reverseFastqs) {
+        int lineNum = 0;
+        
+        try {
+            BufferedReader br;
+            FastqReader fr;
+            
+            for (String fastq : forwardFastqs) {
+                System.out.println("Parsing `" + fastq + "`...");
+                
+                if (fastq.endsWith(GZIP_EXTENSION)) {
+                    br = new BufferedReader(new InputStreamReader(new GZIPInputStream(new FileInputStream(fastq))));
+                }
+                else {
+                    br = new BufferedReader(new InputStreamReader(new FileInputStream(fastq)));
+                }
+                fr = new FastqReader(br, false);
+                while (fr.hasNext()) {
+                    if (++lineNum % 100000 == 0) {
+                        System.out.println("Parsed " + lineNum + " reads...");
+                    }
+
+                    for (String seq : filterFastq(fr.next(), qualPattern)) {
+                        graph.addKmersFromSeq(seq);
+                    }
+                }
+                br.close();
+            }
+            
+            for (String fastq : reverseFastqs) {
+                System.out.println("Parsing `" + fastq + "`...");
+                
+                if (fastq.endsWith(GZIP_EXTENSION)) {
+                    br = new BufferedReader(new InputStreamReader(new GZIPInputStream(new FileInputStream(fastq))));
+                }
+                else {
+                    br = new BufferedReader(new InputStreamReader(new FileInputStream(fastq)));
+                }
+                fr = new FastqReader(br, false);
+                while (fr.hasNext()) {
+                    if (++lineNum % 100000 == 0) {
+                        System.out.println("Parsed " + lineNum + " reads...");
+                    }
+
+                    for (String seq : filterFastq(fr.next(), qualPattern)) {
+                        graph.addKmersFromSeq(reverseComplement(seq));
+                    }
+                }
+                br.close();
+            }
+        }
+        catch (NoSuchElementException e) {
+            /**@TODO handle invalid format*/
+        }
+        catch (IOException e) {
+            /**@TODO Handle it!!! */
+        }
+        finally {
+            System.out.println("Parsed " + lineNum + " reads...");
+        }
+    }
+        
+    public void assembleFragments(String leftFastq, String rightFastq, boolean revCompLeft, boolean revCompRight, int mismatchesAllowed, int bound, int lookahead, int minOverlap) {
+        BufferedReader lbr, rbr;
+        FastqReader leftReader, rightReader;
+
+        try {
+            if (leftFastq.endsWith(GZIP_EXTENSION)) {
+                lbr = new BufferedReader(new InputStreamReader(new GZIPInputStream(new FileInputStream(leftFastq))));
+            }
+            else {
+                lbr = new BufferedReader(new InputStreamReader(new FileInputStream(leftFastq)));
+            }
+            leftReader = new FastqReader(lbr, true);
+
+            if (rightFastq.endsWith(GZIP_EXTENSION)) {
+                rbr = new BufferedReader(new InputStreamReader(new GZIPInputStream(new FileInputStream(rightFastq))));
+            }
+            else {
+                rbr = new BufferedReader(new InputStreamReader(new FileInputStream(rightFastq)));
+            }
+            rightReader = new FastqReader(rbr, true);
+            
+            FastqPairReader fqpr = new FastqPairReader(leftReader, rightReader, qualPattern, revCompLeft, revCompRight);
+            ReadPair p;
+            while (fqpr.hasNext()) {
+                p = fqpr.next();
+                
+                System.out.println("LEFT:  " + p.left);
+                System.out.println("RIGHT: " + p.right);
+                
+                String fragment = assembleFragment(p.left, p.right, graph, mismatchesAllowed, bound, lookahead, minOverlap);
+                
+                System.out.println(fragment);
+                /**@TODO*/
+            }
+            
+            lbr.close();
+            rbr.close();
+        } catch (IOException ex) {
+            //Logger.getLogger(RNABloom.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
     
     /**
      * @param args the command line arguments
@@ -70,93 +186,39 @@ public class RNABloom {
         System.out.println( SequenceOperations.filterFastq(fq, p).toString() );
         */
         
-                
-        long dbgbfSize = NUM_BITS_1GB;
-        int cbfSize = (int) NUM_BYTES_1GB;
-        int dbgbfNumHash = 3;
-        int cbfNumHash = 4;
-        int seed = 689;
-        int k = 25;
-        int q = 3;
         
-        BloomFilterDeBruijnGraph graph = new BloomFilterDeBruijnGraph(dbgbfSize,
-                                                                    cbfSize,
-                                                                    dbgbfNumHash,
-                                                                    cbfNumHash,
-                                                                    seed,
-                                                                    k,
-                                                                    true);
+        String fastq1 = "/projects/btl2/kmnip/rna-bloom/tests/GAPDH_1.fq.gz"; //right
+        String fastq2 = "/projects/btl2/kmnip/rna-bloom/tests/GAPDH_2.fq.gz"; //left
         
-        Pattern qualPattern = getPhred33Pattern(q, k);
-        
-        String fastq1 = "/projects/btl2/kmnip/rna-bloom/tests/GAPDH_1.fq.gz";
-        String fastq2 = "/projects/btl2/kmnip/rna-bloom/tests/GAPDH_2.fq.gz";
-
-        int lineNum = 0;
-        
-        try {
-            BufferedReader br = new BufferedReader(new InputStreamReader(new GZIPInputStream(new FileInputStream(fastq1))));
-            FastqReader fr = new FastqReader(br);
-            Stream<FastqRecord> s = Stream.generate(fr);
-            Iterator<FastqRecord> itr = s.iterator();
-            try {
-                while (true) {
-                    if (++lineNum % 100000 == 0) {
-                        System.out.println("Parsed " + lineNum + " reads...");
-                    }
-
-                    for (String seq : filterFastq(itr.next(), qualPattern)) {
-                        for (String kmer : kmerize(reverseComplement(seq), k)) {
-                            graph.add(kmer);
-                        }
-                    }
-                }
-            }
-            catch (NoSuchElementException e) {
-                //do nothing
-            }
-            
-            br = new BufferedReader(new InputStreamReader(new GZIPInputStream(new FileInputStream(fastq2))));
-            fr = new FastqReader(br);
-            s = Stream.generate(fr);
-            itr = s.iterator();
-            try {
-                while (true) {
-                    if (++lineNum % 100000 == 0) {
-                        System.out.println("Parsed " + lineNum + " reads...");
-                    }
-
-                    for (String seq : filterFastq(itr.next(), qualPattern)) {
-                        for (String kmer : kmerize(seq, k)) {
-                            graph.add(kmer);
-                        }
-                    }
-                }
-            }
-            catch (NoSuchElementException e) {
-                //do nothing
-            }
-        }
-        catch (IOException e) {
-            
-        }
-        finally {
-            System.out.println("Parsed " + lineNum + " reads...");
-        }
-        
-
-        String read1 = "TCGCTGTTGAAGTCAGAGGAGACCACCTGGTGCTCAGTGTAGCCCAGGATGCCCTTGAGGGGGCCCTCCGACGCCTGCTTCACCACCTTCTTGATGTCAT";
-        String read2 = "ACGGGAAGCTCACTGGCATGGCCTTCCGTGTCCCCACTGCCAACGTGTAAGTGGTGGACCTGACCTGCCGTCTAGAAAAACCTGCCAAATATGATGACAT";
-        read1 = reverseComplement(read1);
-        
+        boolean revCompLeft = false;
+        boolean revCompRight = true;
         int mismatchesAllowed = 5;
         int bound = 500;
         int lookahead = 5;
         int minOverlap = 10;
         
-        String fragment = assembleFragment(read2, read1, graph, mismatchesAllowed, bound, lookahead, minOverlap);
-        System.out.println(fragment);
+        String[] forwardFastqs = new String[]{fastq2};
+        String[] backwardFastqs = new String[]{fastq1};
         
+        boolean strandSpecific = true;
+        long dbgbfSize = NUM_BITS_1GB;
+        long cbfSize = NUM_BYTES_1GB;
+        int dbgbfNumHash = 3;
+        int cbfNumHash = 4;
+        int seed = 689;
+        int k = 25;
+        int q = 3;
+                
+                
+        RNABloom assembler = new RNABloom(strandSpecific, dbgbfSize, cbfSize, dbgbfNumHash, cbfNumHash, seed, k, q);
+        assembler.createDBG(forwardFastqs, backwardFastqs);
+        
+        //String left = "AAGGTCATCCCTGAGCTGAACGGGAAGCTCACTGGCA";
+        //String right = "GGGCTACACTGAGCACCAGGTGGTCTCCTCTGACTTCAACAGCGACCCCCCCTCCTCCACCTTTGACGCTGGGGCTGGCATTGCCCTCAACGACCACTTT";
+        
+        
+        
+        //assembler.assembleFragments(fastq2, fastq1, revCompLeft, revCompRight, mismatchesAllowed, bound, lookahead, minOverlap);
     }
     
 }
