9047f2a08d61f1028e50aa953ea61c7c48d2a226
kmnip <kamingnip@gmail.com>
2016-08-30 11:00:28 -0700

    methods to add both single key and paired keys to bloom filter

--- src/rnabloom/bloom/PairedKeysBloomFilter.java
+++ src/rnabloom/bloom/PairedKeysBloomFilter.java
@@ -23,14 +23,23 @@ public class PairedKeysBloomFilter extends BloomFilter {
         this.numHash2 = pairedKeysNumHash;
         this.hashFunction2 = pairedKeysHashFunction;
     }
-
+    
     public void addPair(final String key1, final String key2) {
         long[] hashVals = hashFunction2.getHashValues(key1 + key2);
         for (int h=0; h<numHash2; ++h) {
             bitArray.set(hashVals[h] % size);
         }
     }
-    
+
+    public void addSingleAndPair(final String key1, final String key2) {
+        this.add(key1);
+        this.add(key2);
+        long[] hashVals = hashFunction2.getHashValues(key1 + key2);
+        for (int h=0; h<numHash2; ++h) {
+            bitArray.set(hashVals[h] % size);
+        }
+    }
+        
     public boolean lookupPair(final String key1, final String key2) {
         long[] hashVals = hashFunction2.getHashValues(key1 + key2);
         for (int h=0; h<numHash2; ++h) {
@@ -42,6 +51,10 @@ public class PairedKeysBloomFilter extends BloomFilter {
         return true;
     }
 
+    public boolean lookupSingleAndPair(final String key1, final String key2) {
+        return this.lookup(key1) && this.lookup(key2) && this.lookupPair(key1, key2);
+    }
+    
     public float getPairedFPR() {
         return (float) pow(1 - exp(-numHash2 * bitArray.popCount() / size), numHash2);
     }
