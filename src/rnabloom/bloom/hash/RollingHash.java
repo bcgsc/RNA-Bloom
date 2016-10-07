10d6486bf8182d0465f9c30d4d093e479dddf473
kmnip <kamingnip@gmail.com>
2016-10-06 23:54:52 -0700

    minor edit

---
+++ src/rnabloom/bloom/hash/RollingHash.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rnabloom.bloom.hash;
+
+/**
+ *
+ * @author gengar
+ */
+public class RollingHash {
+    private final int k;
+    
+    public RollingHash(int k) {
+        this.k = k;
+    }
+    
+    /**@TODO */
+}
