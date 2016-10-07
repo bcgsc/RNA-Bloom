8dcbef2863d174fe10e2fa10d8d0259182ac3531
kmnip <kamingnip@gmail.com>
2016-10-07 14:59:11 -0700

    rolling hash

--- src/rnabloom/bloom/hash/RollingHash.java
+++ src/rnabloom/bloom/hash/RollingHash.java
@@ -7,13 +7,115 @@ package rnabloom.bloom.hash;
 
 /**
  *
- * @author gengar
+ * @author kmnip
  */
 public class RollingHash {
+    
+    private static final long SEED_A = 0x3c8bfbb395c60474L;
+    private static final long SEED_C = 0x3193c18562a02b4cL;
+    private static final long SEED_G = 0x20323ed082572324L;
+    private static final long SEED_T = 0x295549f54be24456L;
+        
+    private static final long[] ROLL_A = getRollArray(SEED_A);
+    private static final long[] ROLL_C = getRollArray(SEED_C);
+    private static final long[] ROLL_G = getRollArray(SEED_G);
+    private static final long[] ROLL_T = getRollArray(SEED_T);
+    private static final int ROLL_SIZE = Long.SIZE;
+    
     private final int k;
     
     public RollingHash(int k) {
         this.k = k;
+    }
+    
+    private static long rollLeft(long val, int pos) {
+        return val << pos | val >> (ROLL_SIZE - pos);
+    }
+
+    private static long rollRight(long val, int pos) {
+        return val >> pos | val << (ROLL_SIZE - pos);
+    }
+    
+    private static long[] getRollArray(final long seed) {
+        long[] arr = new long[ROLL_SIZE];
+        
+        for (int i=0; i<ROLL_SIZE; ++i) {
+            arr[i] = rollRight(seed, i);
+        }
+        
+        return arr;
+    }
+    
+    private static long getNucleotideHash(char c) {
+        switch (c) {
+            case 'A':
+                return SEED_A;
+            case 'C':
+                return SEED_C;
+            case 'G':
+                return SEED_G;
+            case 'T':
+                return SEED_T;
+        }
+        return 0;
+    }
+    
+    private static long getNucleotideHash(char c, int pos) {
+        switch (c) {
+            case 'A':
+                return ROLL_A[pos % 64];
+            case 'C':
+                return ROLL_C[pos % 64];
+            case 'G':
+                return ROLL_G[pos % 64];
+            case 'T':
+                return ROLL_T[pos % 64];
+        }
+        return 0;
+    }
+
+    public long getHashValue(final CharSequence kmer) {
+        long hashVal = 0L;
+        
+        for (int i=0; i<k; ++i) {
+            hashVal ^= getNucleotideHash(kmer.charAt(i), i);
+        }
+        
+        return hashVal;
+    }
+    
+    public long getHashValue(final CharSequence kmer, int offset) {
+        long hashVal = 0L;
+        
+        for (int i=0; i<k; ++i) {
+            hashVal ^= getNucleotideHash(kmer.charAt(offset+i), i);
+        }
+        
+        return hashVal;
+    }
+    
+    public long getRightHashValue(long hashVal, char remove, char add) {
+        return rollLeft(hashVal ^ getNucleotideHash(remove), 1) ^ getNucleotideHash(add, k-1);
+    }
+    
+    public long getLeftHashValue(long hashVal, char remove, char add) {
+        return getNucleotideHash(add) ^ rollRight(hashVal ^ getNucleotideHash(remove, k-1), 1);
+    }
+    
+    public long[] getHashValues(final CharSequence seq) {
+        int numKmers = seq.length() - k + 1;
+        
+        long[] out = new long[numKmers];
+        
+        long hashVal = getHashValue(seq);
+        out[0] = hashVal;
+        
+        for (int i=1; i<numKmers; ++i) {
+            hashVal = getRightHashValue(hashVal, seq.charAt(i-1), seq.charAt(i+k-1));
+            out[i] = hashVal;
+        }
+        
+        return out;
     }
     
     /**@TODO */
