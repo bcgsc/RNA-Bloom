e6e5a7f7fa5d6bba6378eeabf7b83f2a6d4f5fb3
kmnip <kamingnip@gmail.com>
2017-05-11 16:34:37 -0700

    half default memory to account for quality lines

--- src/rnabloom/RNABloom.java
+++ src/rnabloom/RNABloom.java
@@ -2242,7 +2242,7 @@ public class RNABloom {
             int qDBG = Integer.parseInt(line.getOptionValue(optBaseQualDbg.getOpt(), "3"));
             int qFrag = Integer.parseInt(line.getOptionValue(optBaseQualFrag.getOpt(), "3"));
                         
-            float maxBfMem = (float) Float.parseFloat(line.getOptionValue(optAllMem.getOpt(), Float.toString((float) ((double) new File(fastqLeft).length() / NUM_BYTES_1GB))));
+            float maxBfMem = (float) Float.parseFloat(line.getOptionValue(optAllMem.getOpt(), Float.toString((float) (new File(fastqLeft).length() * 0.5d / NUM_BYTES_1GB))));
             float sbfGB = Float.parseFloat(line.getOptionValue(optSbfMem.getOpt(), Float.toString(maxBfMem * 0.5f / 7f)));
             float dbgGB = Float.parseFloat(line.getOptionValue(optDbgbfMem.getOpt(), Float.toString(maxBfMem * 1f / 7f)));
             float cbfGB = Float.parseFloat(line.getOptionValue(optCbfMem.getOpt(), Float.toString(maxBfMem * 5f / 7f)));
