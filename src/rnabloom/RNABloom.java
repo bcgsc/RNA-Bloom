8e34d9ad16529546703871f8bb2c6e0bea428b29
kmnip <kamingnip@gmail.com>
2017-05-16 17:07:13 -0700

    adjust for default bf size

--- src/rnabloom/RNABloom.java
+++ src/rnabloom/RNABloom.java
@@ -2263,10 +2263,10 @@ public class RNABloom {
             }
             
             float maxBfMem = (float) Float.parseFloat(line.getOptionValue(optAllMem.getOpt(), Float.toString((float) (leftReadFilesTotalBytes * 0.8d / NUM_BYTES_1GB))));
-            float sbfGB = Float.parseFloat(line.getOptionValue(optSbfMem.getOpt(), Float.toString(maxBfMem * 0.5f / 9f)));
-            float dbgGB = Float.parseFloat(line.getOptionValue(optDbgbfMem.getOpt(), Float.toString(maxBfMem * 1f / 9f)));
-            float cbfGB = Float.parseFloat(line.getOptionValue(optCbfMem.getOpt(), Float.toString(maxBfMem * 7f / 9f)));
-            float pkbfGB = Float.parseFloat(line.getOptionValue(optPkbfMem.getOpt(), Float.toString(maxBfMem * 0.5f / 9f)));
+            float sbfGB = Float.parseFloat(line.getOptionValue(optSbfMem.getOpt(), Float.toString(maxBfMem * 0.5f / 7f)));
+            float dbgGB = Float.parseFloat(line.getOptionValue(optDbgbfMem.getOpt(), Float.toString(maxBfMem * 1f / 7f)));
+            float cbfGB = Float.parseFloat(line.getOptionValue(optCbfMem.getOpt(), Float.toString(maxBfMem * 5f / 7f)));
+            float pkbfGB = Float.parseFloat(line.getOptionValue(optPkbfMem.getOpt(), Float.toString(maxBfMem * 0.5f / 7f)));
             
             long sbfSize = (long) (NUM_BITS_1GB * sbfGB);
             long dbgbfSize = (long) (NUM_BITS_1GB * dbgGB);
