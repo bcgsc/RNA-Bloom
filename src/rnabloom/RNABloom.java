19799f1e2380e183031e57bbe29aa6bf5baa4b8c
kmnip <kamingnip@gmail.com>
2017-10-24 15:25:24 -0700

    include exhaustive search for rescuing read pairs

--- src/rnabloom/RNABloom.java
+++ src/rnabloom/RNABloom.java
@@ -1275,7 +1275,7 @@ public class RNABloom {
                 
                 if (!leftKmers.isEmpty() && !rightKmers.isEmpty()) {
 
-                    ArrayList<Kmer2> fragmentKmers = overlapAndConnect(leftKmers, rightKmers, graph, bound, lookahead, minOverlap, maxCovGradient, false);
+                    ArrayList<Kmer2> fragmentKmers = overlapAndConnect(leftKmers, rightKmers, graph, bound, lookahead, minOverlap, maxCovGradient, true);
 
                     if (fragmentKmers != null) {
                         int fragLength = fragmentKmers.size() + k - 1;
