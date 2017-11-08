16ba3c2b7d9e8e2e47aa43db3faffdbdf782bc23
kmnip <kamingnip@gmail.com>
2017-11-08 13:10:17 -0800

    class representing sequence segments from a read pair

---
+++ src/rnabloom/io/PairedReadSegments.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rnabloom.io;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author kmnip
+ */
+public class PairedReadSegments {
+    public ArrayList<String> left; // left segments
+    public ArrayList<String> right; // right segments
+}
