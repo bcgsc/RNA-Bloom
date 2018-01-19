294b20c30a8c2b0e690a586574457a7aaa0115de
kmnip <kamingnip@gmail.com>
2018-01-19 11:16:59 -0800

    iterator for hashing reverse complement of k-mer pairs

---
+++ src/rnabloom/bloom/hash/ReverseComplementPairedNTHashIterator.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rnabloom.bloom.hash;
+
+import java.util.Arrays;
+import static rnabloom.bloom.hash.HashFunction.combineHashValues;
+import static rnabloom.bloom.hash.NTHash.NTM64;
+import static rnabloom.bloom.hash.NTHash.NTM64RC;
+
+/**
+ *
+ * @author kmnip
+ */
+public class ReverseComplementPairedNTHashIterator extends PairedNTHashIterator {
+
+    public ReverseComplementPairedNTHashIterator(int k, int h, int d) {
+        super(k, h, d);
+    }
+    
+    @Override
+    public void next() {
+        if (pos == -1) {
+            NTM64RC(seq, k, h, 0, hVals1);
+            NTM64RC(seq, k, h, d, hVals2);
+            NTM64(combineHashValues(hVals2[0], hVals1[0]), hVals3, k, h);
+            ++pos;
+        }
+        else if (pos < max) {
+            NTM64RC(seq.charAt(pos), seq.charAt(pos+k), k, h, hVals1);
+            NTM64RC(seq.charAt(pos+d), seq.charAt(pos+k+d), k, h, hVals2);
+            NTM64(combineHashValues(hVals2[0], hVals1[0]), hVals3, k, h);
+            ++pos;
+        }
+        else {
+            Arrays.fill(hVals1, 0);
+            Arrays.fill(hVals2, 0);
+            Arrays.fill(hVals3, 0);
+            hVals1 = null;
+            hVals2 = null;
+            hVals3 = null;
+        }
+    }
+}
