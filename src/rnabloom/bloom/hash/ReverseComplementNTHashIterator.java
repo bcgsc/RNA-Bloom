d7f8da431fb74b020187f2e47bb5f65d05683e82
kmnip <kamingnip@gmail.com>
2018-01-22 17:54:39 -0800

    fix bug in hashing first k-mer pair

--- src/rnabloom/bloom/hash/ReverseComplementNTHashIterator.java
+++ src/rnabloom/bloom/hash/ReverseComplementNTHashIterator.java
@@ -6,7 +6,6 @@
 package rnabloom.bloom.hash;
 
 import static rnabloom.bloom.hash.NTHash.NTM64RC;
-import static rnabloom.bloom.hash.NTHash.NTM64RC;
 
 /**
  *
@@ -19,15 +18,15 @@ public class ReverseComplementNTHashIterator extends NTHashIterator {
 
     @Override
     public void next() {
-        if (pos == -1) {
-            NTM64RC(seq.subSequence(0, k), k, h, hVals);
+        if (pos == start) {
+            NTM64RC(seq, k, h, ++pos, hVals);
         }
-        else if (pos >= 0) {
+        else if (pos < max) {
             NTM64RC(seq.charAt(pos), seq.charAt(pos+k), k, h, hVals);
+            ++pos;
         }
         else {
             hVals = null;
         }
-        ++pos;
     }
 }
