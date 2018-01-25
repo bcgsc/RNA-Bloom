a6834f0a5ae0444e7018b0f312e5d5c30c912abd
kmnip <kamingnip@gmail.com>
2018-01-24 22:19:41 -0800

    E0 fragments are not part of sensitive mode anymore

--- src/rnabloom/RNABloom.java
+++ src/rnabloom/RNABloom.java
@@ -2966,7 +2966,7 @@ public class RNABloom {
             
             // extend LONG fragments
             
-            for (int mag=longFragmentsFastas.length-1; mag>0; --mag) {
+            for (int mag=longFragmentsFastas.length-1; mag>=0; --mag) {
                 writer.setOutputPrefix(txptNamePrefix + "E" + mag + ".L.");
                 String fragmentsFasta = longFragmentsFastas[mag];
                 System.out.println("Parsing `" + fragmentsFasta + "`...");
@@ -2976,7 +2976,7 @@ public class RNABloom {
 
             // extend SHORT fragments
             
-            for (int mag=shortFragmentsFastas.length-1; mag>0; --mag) {
+            for (int mag=shortFragmentsFastas.length-1; mag>=0; --mag) {
                 writer.setOutputPrefix(txptNamePrefix + "E" + mag + ".S.");
                 String fragmentsFasta = shortFragmentsFastas[mag];
                 System.out.println("Parsing `" + fragmentsFasta + "`...");
@@ -2986,7 +2986,7 @@ public class RNABloom {
             
             // extend UNCONNECTED reads
             
-            for (int mag=unconnectedReadsFastas.length-1; mag>0; --mag) {
+            for (int mag=unconnectedReadsFastas.length-1; mag>=0; --mag) {
                 writer.setOutputPrefix(txptNamePrefix + "E" + mag + ".U.");
                 String fragmentsFasta = unconnectedReadsFastas[mag];
                 System.out.println("Parsing `" + fragmentsFasta + "`...");
@@ -3004,6 +3004,7 @@ public class RNABloom {
                 extendBranchFreeOnly = true;
             }
             
+            /*
             // extend LONG fragments
             
             writer.setOutputPrefix(txptNamePrefix + "E0.L.");
@@ -3027,7 +3028,7 @@ public class RNABloom {
             System.out.println("Parsing `" + fragmentsFasta + "`...");
             numFragmentsParsed += extendFragmentsMultiThreadedHelper(fragmentsFasta, writer, sampleSize, numThreads,
                                                                         allowNaiveExtension, extendBranchFreeOnly);
-
+            */
             
             // extend LONG singleton fragments
 
