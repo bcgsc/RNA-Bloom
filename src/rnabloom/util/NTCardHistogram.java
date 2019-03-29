9481c4d89d140b3e98d9425fcdb36e3bb44ccf35
kmnip <kamingnip@gmail.com>
2019-03-29 14:35:07 -0700

    fix bug in finding coverage threshold in ntCard histogram

--- src/rnabloom/util/NTCardHistogram.java
+++ src/rnabloom/util/NTCardHistogram.java
@@ -39,6 +39,9 @@ public class NTCardHistogram {
                         covThreshold = Integer.parseInt(cols[0]);
                         break;
                     }
+                    else {
+                        lastCount = count;
+                    }
                 }
             }
         }
