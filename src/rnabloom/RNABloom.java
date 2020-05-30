b01a3bc3038ca9050c761eded20a9d8608983433
kmnip <kamingnip@gmail.com>
2020-05-29 22:26:30 -0700

    bump version number

--- src/rnabloom/RNABloom.java
+++ src/rnabloom/RNABloom.java
@@ -91,7 +91,7 @@ import rnabloom.io.SequenceFileIteratorInterface;
  * @author Ka Ming Nip
  */
 public class RNABloom {
-    public final static String VERSION = "1.3.0";
+    public final static String VERSION = "1.3.1";
     
 //    private final static long NUM_PARSED_INTERVAL = 100000;
     public final static long NUM_BITS_1GB = (long) pow(1024, 3) * 8;
