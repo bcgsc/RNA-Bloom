39c5a869eb417bba2b3722b6e6cac9989b4f6d25
kmnip <kamingnip@gmail.com>
2021-03-07 00:23:55 -0800

    extract interval classes

---
+++ src/rnabloom/olc/Interval.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rnabloom.olc;
+
+/**
+ *
+ * @author Ka Ming Nip
+ */
+public class Interval {
+    public int start;
+    public int end;
+
+    public Interval(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+}
--- src/rnabloom/olc/Layout.java
+++ src/rnabloom/olc/Layout.java
@@ -454,53 +454,6 @@ public class Layout {
         
         return sb.toString();
     }
-        
-    private static class Interval {
-        public int start;
-        public int end;
-        
-        public Interval(int start, int end) {
-            this.start = start;
-            this.end = end;
-        }
-    }
-    
-    private static class ComparableInterval extends Interval implements Comparable<Interval>{        
-        public ComparableInterval(int start, int end) {
-            super(start, end);
-        }
-        
-        public boolean merge(Interval other) {
-            if (start >= other.start && start <= other.end) {
-                start = other.start;
-                end = Math.max(end, other.end);
-                return true;
-            }
-            else if (end >= other.start && end <= other.end) {
-                start = Math.min(start, other.start);
-                end = other.end;
-                return true;
-            }
-            else if (other.start >= start && other.start <= end) {
-                end = Math.max(end, other.end);
-                return true;
-            }
-            else if (other.end >= start && other.end <= end) {
-                start = Math.min(start, other.start);
-                return true;
-            }
-            return false; // not merged
-        }
-        
-        @Override
-        public int compareTo(Interval other) {
-            int c = start - other.start;
-            if (c == 0) {
-                c = end - other.end;
-            }
-            return c;
-        }
-    }
 
     /*
     private static Interval overlapInterval(Interval span1, Interval span2) {
