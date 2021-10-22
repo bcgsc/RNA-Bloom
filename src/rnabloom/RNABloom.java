dec2766c70fd0ebb9ee137db3713f9116f8004e6
kmnip <kamingnip@gmail.com>
2021-10-22 10:43:23 -0700

    bump version

--- src/rnabloom/RNABloom.java
+++ src/rnabloom/RNABloom.java
@@ -111,7 +111,7 @@ import rnabloom.util.WeightedBitSequence;
  * @author Ka Ming Nip
  */
 public class RNABloom {
-    public final static String VERSION = "1.4.3";
+    public final static String VERSION = "1.4.4";
     
 //    private final static long NUM_PARSED_INTERVAL = 100000;
     public final static long NUM_BITS_1GB = (long) pow(1024, 3) * 8;
