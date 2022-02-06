e018fe044417777d151d701b9b7b51b30968edc4
kmnip <kamingnip@gmail.com>
2022-02-06 14:35:01 -0800

    increase default buffer size

--- src/rnabloom/io/Constants.java
+++ src/rnabloom/io/Constants.java
@@ -13,5 +13,5 @@ public class Constants {
     public final static String FASTA_EXT = ".fa";
     public final static String GZIP_EXT = ".gz";
     public final static String NBITS_EXT = ".nbits";
-    public final static int BUFFER_SIZE = 1024 * 8;
+    public final static int BUFFER_SIZE = 1024 * 128;
 }
