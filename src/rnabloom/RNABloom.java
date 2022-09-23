f767ee6283a5b858cb7a1a94b197bde173ec7ad2
kmnip <kamingnip@gmail.com>
2022-09-23 11:07:45 -0700

    bump version

--- src/rnabloom/RNABloom.java
+++ src/rnabloom/RNABloom.java
@@ -110,7 +110,7 @@ import static rnabloom.util.FileUtils.writeIntArrayToFile;
  * @author Ka Ming Nip
  */
 public class RNABloom {
-    public final static String VERSION = "2.0.0";
+    public final static String VERSION = "2.0.1";
     
 //    private final static long NUM_PARSED_INTERVAL = 100000;
     public final static long NUM_BITS_1GB = (long) pow(1024, 3) * 8;
